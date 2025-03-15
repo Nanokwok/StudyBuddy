166b596511a9ad81b69a309cc06218efd142a4f5
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2025-03-16 00:57:48 +0700

    Initial commit

---
+++ StudyBuddy/StudyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyApp.swift
+//  StudyBuddy
+//
+//  Created by Nano Kwok on 16/3/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct StudyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
