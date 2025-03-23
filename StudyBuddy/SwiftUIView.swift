cbed0081ccc07b9c3bd660087253b3c466fdeba7
1stChaS <chananthida.so@ku.th>
2025-03-23 14:00:16 +0700

    Course and Login view

---
+++ StudyBuddy/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  StudyBuddy
+//
+//  Created by Chananthida Sopaphol on 22/3/2568 BE.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
